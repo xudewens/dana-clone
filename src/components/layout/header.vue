9957f91cd9a86bcbd51a5f074b5a1b4bebe9a52a
xiejun <xiejun@keeprisk.com>
2025-05-27 18:07:30 +0800

    style:样式修复

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -2,7 +2,7 @@
  * @Author: xiejun xiejun@keeprisk.com
  * @Date: 2025-05-23 18:29:22
  * @LastEditors: xiejun xiejun@keeprisk.com
- * @LastEditTime: 2025-05-23 18:29:55
+ * @LastEditTime: 2025-05-27 18:07:04
  * @FilePath: /dana-clone/src/components/layout/header.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -88,7 +88,8 @@ export default {};
 
     .header__header-box .content-left {
       margin-left: 0;
-      top: 0.06rem;
+      // top: 0.06rem;
+      top: 0.02rem;
     }
 
     .header__header-box .content-right {
