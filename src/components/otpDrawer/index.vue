73dc778de89934b7005f9f6d34899d194dc7464c
xiejun <xiejun@keeprisk.com>
2025-05-27 22:49:18 +0800

    feat: 增加重新发送otp code 弹窗

--- src/components/otpDrawer/index.vue
+++ src/components/otpDrawer/index.vue
@@ -2,7 +2,7 @@
  * @Author: xiejun xiejun@keeprisk.com
  * @Date: 2025-05-27 21:58:00
  * @LastEditors: xiejun xiejun@keeprisk.com
- * @LastEditTime: 2025-05-27 22:27:39
+ * @LastEditTime: 2025-05-27 22:47:04
  * @FilePath: /dana-clone/src/components/otpDrawer/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -47,15 +47,51 @@
         </el-button>
       </div>
     </PinCodeDrawer>
+    <CustomerDrawer
+      :visible.sync="showDrawer"
+      :hasHeader="true"
+      :showClose="true"
+      :size="'60%'"
+      :title="'Resend OTP code?'"
+    >
+      <div></div>
+      <!-- 主要内容 -->
+      <div class="help-core">
+        <div class="help-core__wrap">
+          <div class="help-core__header">
+            <img
+              src="https://a.m.dana.id/resource/imgs/ipg/blue-forgot-pin-square-background.svg"
+              alt="help"
+            />
+          </div>
+          <div class="help-core__content">
+            <div class="help-core__title">Resend OTP code?</div>
+            <div class="help-core__desc">
+              A new code will be sent to +62881*****4017
+            </div>
+          </div>
+          <div class="help-core__button-container">
+            <el-button
+              type="primary"
+              class="help-core__button"
+              @click="showDrawer = false"
+              >RESEND CODE</el-button
+            >
+          </div>
+        </div>
+      </div>
+    </CustomerDrawer>
   </div>
 </template>
 
 <script>
 import PinCodeDrawer from "@/components/common/actionDrawer.vue";
+import CustomerDrawer from "../common/customerDrawer.vue";
 
 export default {
   components: {
     PinCodeDrawer,
+    CustomerDrawer,
   },
   computed: {
     // 动态按钮文字
@@ -65,7 +101,8 @@ export default {
   },
   data() {
     return {
-      showOtpDrawer: true,
+      showOtpDrawer: false,
+      showDrawer: true,
       showKeyboard: false,
       otpCode: "",
       account: "+6288134254325",
@@ -116,35 +153,44 @@ export default {
   },
 };
 </script>
-
 <style lang="scss" scoped>
-.na-otp-drawer {
-  .otp_code_container {
-    text-align: center;
-    padding: 0.2rem;
-    box-sizing: border-box;
-    // margin-top: 0.2rem;
-    .title {
-      font-weight: 600;
-    }
-    .send_desc {
-      //   margin-bottom: 0.07rem;
-    }
-    .otpCode_input {
-      width: 80%;
-      margin: auto;
-    }
-    .resend_btn {
-      margin-top: 0.1rem;
-    }
-    .otp_type {
-      font-weight: 600;
-      color: #418ce3;
-      margin-bottom: 0.07rem;
-    }
+.help-core__content {
+  padding: 0.12rem 0.16rem 0;
+  text-align: center;
+}
+.help-core__button {
+  background-color: #108ee9;
+  border: 0;
+  border-radius: 0.04rem;
+  color: #fff;
+  cursor: pointer;
+  font-size: 0.16rem;
+  font-weight: 600;
+  padding: 0.16rem;
+  width: 100%;
+}
+@media (max-width: 480px) {
+  .help-core__title {
+    font-size: 0.2rem;
+    line-height: 0.26rem;
+    margin-bottom: 0.1rem;
   }
-  ::v-deep .van-password-input__security li {
-    border: 1px solid #ebedf0;
+  .help-core__desc {
+    font-size: 0.14rem;
+    line-height: 0.22rem;
+    margin-bottom: 0.1rem;
+  }
+  .help-core__button-container {
+    padding: 0 0.17rem 0.2rem;
+  }
+  ::v-deep .el-drawer__header {
+    margin-bottom: 0px;
+  }
+  ::v-deep #el-drawer__title {
+    font-size: 20px;
+    font-weight: 600;
+    border-bottom: 1px solid #ebedf0;
+    color: #000;
   }
 }
 </style>
